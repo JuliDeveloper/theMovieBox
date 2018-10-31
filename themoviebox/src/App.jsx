2050381b5965587c18de94b1ce74a0d50f533469
Julia Romanenko <rja-97@yandex.ru>
2018-10-31 15:01:06 +0300

    added css-reset

---
+++ themoviebox/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return <Frarment>
+    
+  </Frarment>
+}
+
+
+export default App;
