06cdb63b37bb2e3ee1ebcbaf2865e0c0b4ba9fb8
Julia Romanenko <rja-97@yandex.ru>
2018-10-31 18:03:57 +0300

    Added Info-movie

--- themoviebox/src/components/header/Header.jsx
+++ themoviebox/src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import '../../../src/css-reset.css';
 import './Header.css';
-import '../../images/search.svg'
 
 
 function Header() {
@@ -17,24 +17,24 @@ function Header() {
                 <li className="category__name">Fantasy</li>
                 <li className="category__name">Animation</li>
                 <li className="category__name">Family</li>
-                <li className="category__name">Duration: 1h 52m</li>
+                <li className="category__name line">Duration: 1h 52m</li>
             </ul>
             <div className="button">
                 <button className="watch">Watch movie</button>
-                <button>View info</button>
-                <button>+ Add to Wishlist</button>
+                <button className="more-info">View info</button>
+                <button className="add">+ Add to Wishlist</button>
             </div>
             <div className="rating">
                 <h2 className="rating__title">Rating</h2>
                 <span className="rating__info">based on 3.546 reviews</span>
                 <ul className="rating__stars">
-                    <li className="star1"></li>
-                    <li className="star2"></li>
-                    <li className="star3"></li>
-                    <li className="star4"></li>
-                    <li className="star5"></li>
+                    <li className="item star-full"></li>
+                    <li className="item star-full"></li>
+                    <li className="item star-full"></li>
+                    <li className="item star-half"></li>
+                    <li className="item star"></li>
                 </ul>
-                <span>3.4</span>
+                <span className="rating__numbers">3.4</span>
             </div>
         </div>
     </header>
