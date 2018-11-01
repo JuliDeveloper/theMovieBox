61ae632f39b85533be3130974e1ed248b219475f
Julia Romanenko <rja-97@yandex.ru>
2018-11-01 14:26:17 +0300

    Created the basis for the content

--- themoviebox/src/App.jsx
+++ themoviebox/src/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component,Fragment } from 'react';
 import './App.css';
+import Content from './components/content/Content.jsx';
 
 
 function App() {
   return <Fragment>
-    
+      <Content />
   </Fragment>
 }
 
