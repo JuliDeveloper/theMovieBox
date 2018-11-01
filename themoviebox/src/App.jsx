02b652ae9c320364a6b6e56bfa1c68084d8d45d9
Julia Romanenko <rja-97@yandex.ru>
2018-11-01 15:50:13 +0300

    Added liner-gradient on images

--- themoviebox/src/App.jsx
+++ themoviebox/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component,Fragment } from 'react';
 import './App.css';
 import Content from './components/content/Content.jsx';
+import '../src/css-reset.css';
 
 
 function App() {
