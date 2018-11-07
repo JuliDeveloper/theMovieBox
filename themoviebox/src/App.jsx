be3a8fbaffe622227b6637b7c3a85c1ca1d2b4a4
Julia Romanenko <rja-97@yandex.ru>
2018-11-07 20:37:28 +0300

    created new branch footer and new component footer

--- themoviebox/src/App.jsx
+++ themoviebox/src/App.jsx
@@ -1,11 +1,11 @@
 import React, { Component,Fragment } from 'react';
 import './App.css';
 import '../src/css-reset.css';
-
+import Footer from '../src/components/footer.jsx';
 
 function App() {
   return <Fragment>
-    
+      <Footer />
   </Fragment>
 }
 
