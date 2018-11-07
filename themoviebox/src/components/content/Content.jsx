fbea381fa870d825ec7af01e560e4a712a5e556b
Julia Romanenko <rja-97@yandex.ru>
2018-11-07 20:16:10 +0300

    finished the card with the movies, began to make the loading

--- themoviebox/src/components/content/Content.jsx
+++ themoviebox/src/components/content/Content.jsx
@@ -4,139 +4,151 @@ import './Content.css';
 
 function Content() {
     return <div className="content">
-
-        <div className="card">
-            <div className="image">
-                <span className="year">2017</span>
+        <div className="box">
+            <div className="card">
+                <div className="image">
+                    <span className="year">2017</span>
+                </div>
+                <div className="info-movie">
+                    <h1 className="title">Logan</h1>
+                    <h2 className="category">Action, Adventure, Fantasy</h2>
+                    <span className="rating">4.0</span>
+                </div>
             </div>
-            <div className="info-movie">
-                <h1 className="title">Logan</h1>
-                <h2 className="category">Action, Adventure, Fantasy</h2>
-                <span className="rating">4.0</span>
+
+            <div className="card">
+                <div className="image">
+                    <span className="year">2017</span>
+                </div>
+                <div className="info-movie">
+                    <h1 className="title">John Wick: Chapter 2</h1>
+                    <h2 className="category">Action, Thriller</h2>
+                    <span className="rating">4.8</span>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="image">
+                    <span className="year">2017</span>
+                </div>
+                <div className="info-movie">
+                    <h1 className="title">Fantastic beasts an…</h1>
+                    <h2 className="category">Adventure, Fantasy, Action</h2>
+                    <span className="rating">4.5</span>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="image">
+                    <span className="year">2017</span>
+                </div>
+                <div className="info-movie">
+                    <h1 className="title">Fifty shades darker</h1>
+                    <h2 className="category">Drama, Roman</h2>
+                    <span className="rating">4.6</span>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="image">
+                    <span className="year">2017</span>
+                </div>
+                <div className="info-movie">
+                    <h1 className="title">Beauty and the Beast</h1>
+                    <h2 className="category">Musical, Drama, Fantasy</h2>
+                    <span className="rating">4.4</span>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="image">
+                    <span className="year">2017</span>
+                </div>
+                <div className="info-movie">
+                    <h1 className="title">Arrival</h1>
+                    <h2 className="category">Action, Thriller, Sci-Fi</h2>
+                    <span className="rating">3.4</span>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="image">
+                    <span className="year">2017</span>
+                </div>
+                <div className="info-movie">
+                    <h1 className="title">Justice League</h1>
+                    <h2 className="category">Adventure, Sci-Fi, Action</h2>
+                    <span className="rating">3.9</span>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="image">
+                    <span className="year">2017</span>
+                </div>
+                <div className="info-movie">
+                    <h1 className="title">Pirates of the Caribb…</h1>
+                    <h2 className="category">Adventure, Fantasy, Action</h2>
+                    <span className="rating">4.2</span>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="image">
+                    <span className="year">2017</span>
+                </div>
+                <div className="info-movie">
+                    <h1 className="title">Wonder Woman</h1>
+                    <h2 className="category">Action, Adventure, Sci-Fi</h2>
+                    <span className="rating">3.8</span>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="image">
+                    <span className="year">2017</span>
+                </div>
+                <div className="info-movie">
+                    <h1 className="title">Guardians of the Gal…</h1>
+                    <h2 className="category">Action, Sci-Fi, Comedy</h2>
+                    <span className="rating">3.0</span>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="image">
+                    <span className="year">2017</span>
+                </div>
+                <div className="info-movie">
+                    <h1 className="title">Alien: Covenant</h1>
+                    <h2 className="category">Horror, Action, Sci-Fi, Action</h2>
+                    <span className="rating">4.0</span>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="image">
+                    <span className="year">2017</span>
+                </div>
+                <div className="info-movie">
+                    <h1 className="title">Captain Underpant…</h1>
+                    <h2 className="category">Animation, Comedy</h2>
+                    <span className="rating">3.5</span>
+                </div>
             </div>
         </div>
-
-        <div className="card">
-            <div className="image">
-                <span className="year">2017</span>
+        <div className="loading">
+            <div className="line">
+                <div className="line__small"></div>
+                <div className="line__middle"></div>
+                <div className="line__big"></div>
+                <div className="line__small"></div>
+                <div className="line__big"></div>
+                <div className="line__middle"></div>
+                <div className="line__small"></div>
             </div>
-            <div className="info-movie">
-                <h1 className="title">John Wick: Chapter 2</h1>
-                <h2 className="category">Action, Thriller</h2>
-                <span className="rating">4.8</span>
-            </div>
+            <span className="text">loading</span>
         </div>
-
-        <div className="card">
-            <div className="image">
-                <span className="year">2017</span>
-            </div>
-            <div className="info-movie">
-                <h1 className="title">Fantastic beasts an…</h1>
-                <h2 className="category">Adventure, Fantasy, Action</h2>
-                <span className="rating">4.5</span>
-            </div>
-        </div>
-
-        <div className="card">
-            <div className="image">
-                <span className="year">2017</span>
-            </div>
-            <div className="info-movie">
-                <h1 className="title">Fifty shades darker</h1>
-                <h2 className="category">Drama, Roman</h2>
-                <span className="rating">4.6</span>
-            </div>
-        </div>
-
-        <div className="card">
-            <div className="image">
-                <span className="year">2017</span>
-            </div>
-            <div className="info-movie">
-                <h1 className="title">Beauty and the Beast</h1>
-                <h2 className="category">Musical, Drama, Fantasy</h2>
-                <span className="rating">4.4</span>
-            </div>
-        </div>
-
-        <div className="card">
-            <div className="image">
-                <span className="year">2017</span>
-            </div>
-            <div className="info-movie">
-                <h1 className="title">Arrival</h1>
-                <h2 className="category">Action, Thriller, Sci-Fi</h2>
-                <span className="rating">3.4</span>
-            </div>
-        </div>
-
-        <div className="card">
-            <div className="image">
-                <span className="year">2017</span>
-            </div>
-            <div className="info-movie">
-                <h1 className="title">Justice League</h1>
-                <h2 className="category">Adventure, Sci-Fi, Action</h2>
-                <span className="rating">3.9</span>
-            </div>
-        </div>
-
-        <div className="card">
-            <div className="image">
-                <span className="year">2017</span>
-            </div>
-            <div className="info-movie">
-                <h1 className="title">Pirates of the Caribb…</h1>
-                <h2 className="category">Adventure, Fantasy, Action</h2>
-                <span className="rating">4.2</span>
-            </div>
-        </div>
-
-        <div className="card">
-            <div className="image">
-                <span className="year">2017</span>
-            </div>
-            <div className="info-movie">
-                <h1 className="title">Wonder Woman</h1>
-                <h2 className="category">Action, Adventure, Sci-Fi</h2>
-                <span className="rating">3.8</span>
-            </div>
-        </div>
-
-        <div className="card">
-            <div className="image">
-                <span className="year">2017</span>
-            </div>
-            <div className="info-movie">
-                <h1 className="title">Guardians of the Gal…</h1>
-                <h2 className="category">Action, Sci-Fi, Comedy</h2>
-                <span className="rating">3.0</span>
-            </div>
-        </div>
-
-        <div className="card">
-            <div className="image">
-                <span className="year">2017</span>
-            </div>
-            <div className="info-movie">
-                <h1 className="title">Alien: Covenant</h1>
-                <h2 className="category">Horror, Action, Sci-Fi, Action</h2>
-                <span className="rating">4.0</span>
-            </div>
-        </div>
-
-        <div className="card">
-            <div className="image">
-                <span className="year">2017</span>
-            </div>
-            <div className="info-movie">
-                <h1 className="title">Captain Underpant…</h1>
-                <h2 className="category">Animation, Comedy</h2>
-                <span className="rating">3.5</span>
-            </div>
-        </div>
-
     </div>
 }
 
