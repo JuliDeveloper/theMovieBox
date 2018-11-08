7fb96ed1487bfb53a5bc216ef170691c327a07d7
Julia Romanenko <rja-97@yandex.ru>
2018-11-08 16:11:45 +0300

    removed margin from the info-movie class and positioned it

--- themoviebox/src/components/header/Header.jsx
+++ themoviebox/src/components/header/Header.jsx
@@ -12,18 +12,20 @@ function Header() {
             <button className="sing-up">sing up</button>
         </div>
         <div className="info-movie">
-            <h1 className="title">Wrath of the titans</h1>
-            <ul className="category">
-                <li className="category__name">Fantasy</li>
-                <li className="category__name">Animation</li>
-                <li className="category__name">Family</li>
-                <li className="category__name line">Duration: 1h 52m</li>
-            </ul>
-            <div className="button">
-                <button className="watch">Watch movie</button>
-                <button className="more-info">View info</button>
-                <button className="add">+ Add to Wishlist</button>
-            </div>
+            
+                <h1 className="title">Wrath of the titans</h1>
+                <ul className="category">
+                    <li className="category__name">Fantasy</li>
+                    <li className="category__name">Animation</li>
+                    <li className="category__name">Family</li>
+                    <li className="category__name line">Duration: 1h 52m</li>
+                </ul>
+                <div className="button">
+                    <button className="watch">Watch movie</button>
+                    <button className="more-info">View info</button>
+                    <button className="add">+ Add to Wishlist</button>
+                </div>
+           
             <div className="rating">
                 <h2 className="rating__title">Rating</h2>
                 <span className="rating__info">based on 3.546 reviews</span>
