893aae17200c00fcdf17f9b29dcec3dba9429cfc
Julia Romanenko <rja-97@yandex.ru>
2018-11-29 12:44:34 +0300

    removed extra classes

--- themoviebox/src/components/filters/Filters.jsx
+++ themoviebox/src/components/filters/Filters.jsx
@@ -7,9 +7,9 @@ function Filters() {
         <div className="filter__left">
             <ul className="filter__list">
                 <li className="filter__item"><a href="#" className="link one-line">Trending</a></li>
-                <li className="filter__item two-line"><a href="#" className="link">Top Rated</a></li>
-                <li className="filter__item three-line"><a href="#" className="link">New Arrivals</a></li>
-                <li className="filter__item fout-line fall"><a href="#" className="link">Genre</a>
+                <li className="filter__item"><a href="#" className="link">Top Rated</a></li>
+                <li className="filter__item"><a href="#" className="link">New Arrivals</a></li>
+                <li className="filter__item fall"><a href="#" className="link">Genre</a>
                     <ul className="sublist">
                         <li className="subitem"><a href="#" className="link">Interesting</a></li>
                         <li className="subitem"><a href="#" className="link">Interesting</a></li>
