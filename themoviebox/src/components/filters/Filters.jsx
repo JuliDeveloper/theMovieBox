54b65030299f7460aad4abee2a25c268f1dd6602
Julia Romanenko <rja-97@yandex.ru>
2018-11-29 13:08:40 +0300

    renamed class name

--- themoviebox/src/components/filters/Filters.jsx
+++ themoviebox/src/components/filters/Filters.jsx
@@ -3,13 +3,13 @@ import '../../../src/css-reset.css';
 import './Filters.css';
 
 function Filters() {
-    return <div className="filters">
-        <div className="filter__left">
-            <ul className="filter__list">
-                <li className="filter__item"><a href="#" className="link one-line">Trending</a></li>
-                <li className="filter__item"><a href="#" className="link">Top Rated</a></li>
-                <li className="filter__item"><a href="#" className="link">New Arrivals</a></li>
-                <li className="filter__item fall"><a href="#" className="link">Genre</a>
+    return <div className="tab-bar">
+        <div className="tab-bar__left">
+            <ul className="tab-bar__list">
+                <li className="tab-bar__item"><a href="#" className="link one-line">Trending</a></li>
+                <li className="tab-bar__item"><a href="#" className="link">Top Rated</a></li>
+                <li className="tab-bar__item"><a href="#" className="link">New Arrivals</a></li>
+                <li className="tab-bar__item fall"><a href="#" className="link">Genre</a>
                     <ul className="sublist">
                         <li className="subitem"><a href="#" className="link">Interesting</a></li>
                         <li className="subitem"><a href="#" className="link">Interesting</a></li>
@@ -17,7 +17,7 @@ function Filters() {
                 </li>
             </ul>
         </div>
-        <div className="filter__right">
+        <div className="tab-bar__right">
             <div className="square">
                 <span className="square__line"></span>
                 <span className="square__line"></span>
